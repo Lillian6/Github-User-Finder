05a376333206f3474f11fb14ff742f4658f5f6e4
Lillian6 <lp2740@tc.columbia.edu>
2018-10-25 17:08:31 -0400

    second commit

---
+++ main.js
@@ -0,0 +1,77 @@
+
+// var inputValue = document.querySelector('#search');
+// var searchButton = document.querySelector('.search_button');
+// const client_id = "427b5784495338941947";
+// const client_secret = "f128ec8253a914e370277268f3953d0d33982693";
+//var fs = require("fs");
+
+// var canvas = d3.select("body")
+//                 .append("svg")
+//                 .attr("width", 500)
+//                 .attr("height", 500);
+//
+// var circle = canvas.append("circle")
+//                     .attr("cx", 250)
+//                     .attr("cy", 250)
+//                     .attr("r", 50)
+//                     .attr("fill", "pink");
+// const userName = inputValue.value;
+// function getUsers(userName) {
+//   fetch('https://api.github.com/users/${userName}?$client_id="427b5784495338941947"&$client_secret="f128ec8253a914e370277268f3953d0d33982693"')
+//   .then(response => response.json())
+//   .then(data => {
+//     console.log(data) // Prints result from `response.json()` in getRequest
+//   })
+//   // .catch(error => console.error(error))
+// };
+
+
+// searchButton.addEventListener('click', () => {
+//   getUsers();
+// })
+
+// let repoChart = new Chart(CANVAS, {
+//   type: 'line',
+//   data: {
+//    labels: [1500,1600,1700,1750,1800,1850,1900,1950,1999,2050],
+//    datasets: [{
+//        data: [86,114,106,106,107,111,133,221,783,2478],
+//        label: "Africa",
+//        borderColor: "#3e95cd",
+//        fill: false
+//      }, {
+//        data: [282,350,411,502,635,809,947,1402,3700,5267],
+//        label: "Asia",
+//        borderColor: "#8e5ea2",
+//        fill: false
+//      }, {
+//        data: [168,170,178,190,203,276,408,547,675,734],
+//        label: "Europe",
+//        borderColor: "#3cba9f",
+//        fill: false
+//      }, {
+//        data: [40,20,10,16,24,38,74,167,508,784],
+//        label: "Latin America",
+//        borderColor: "#e8c3b9",
+//        fill: false
+//      }, {
+//        data: [6,3,2,2,7,26,82,172,312,433],
+//        label: "North America",
+//        borderColor: "#c45850",
+//        fill: false
+//      }
+//    ]
+//  },
+//  options: {
+//    title: {
+//      display: true,
+//      text: 'World population per region (in millions)'
+// }
+// );
+
+// function getOption()
+// {
+//   var inputYear = document.getElementById(".mySelect");
+
+
+// }
